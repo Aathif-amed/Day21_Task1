e8207a52ae4dc477595ea603efc0630a80e657cd
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2022-12-12 14:52:06 +0530

    added tamil translation

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ div.append(row);
 document.body.append(div);
 
 let col = document.createElement("div");
-col.classList.add("col-6","my-5");
+col.classList.add("col-8","my-5");
 
 
 let random = Math.floor(Math.random() * 6236);
 async function getData() {
   try {
-    const fetchData = await fetch("`http://api.alquran.cloud/v1/ayah/${random}`");
+    const fetchData = await fetch(`http://api.alquran.cloud/v1/ayah/${random}`);
     const fetchDatainenglish = await fetch(`http://api.alquran.cloud/v1/ayah/${random}/en.asad`);
+    const fetchDataintamil = await fetch(`http://api.alquran.cloud/v1/ayah/${random}/ta.tamil`);
     const inArabic = await fetchData.json()
     const inEnglish = await fetchDatainenglish.json()
+    const inTamil = await fetchDataintamil.json()
+
 
     
         col.innerHTML = `
@@ -35,20 +38,34 @@ async function getData() {
         <div class="card-body text-center bg-transparent">
             <div class="card-text text-dark d-flex justify-content-end"> ${inArabic.data.text}</div>
             <div class="card-text text-dark">${inEnglish.data.text}</div>
+            <div class="card-text text-dark">${inTamil.data.text}</div>
             <div class="card-text text-dark d-flex justify-content-end">${inArabic.data.surah.englishName
             } : ${inArabic.data.numberInSurah} </div>
             <div class="card-text text-dark d-flex justify-content-end">Chapter : Verse</div>
         <a onclick="load()" class="btn btn-lg btn-transparent d-flex justify-content-end "><i class="fa-solid fa-rotate-right"></i></a>
-
-          </div>
-    
-     </div>`;
+        
+        </div>
+        
+        </div>`;
         row.append(col);
-    console.log(inArabic.data);
-    console.log(inEnglish.data);
-    // console.log(eng_translation);
-  } catch (error) {
-    console.log(err);
+        
+        // console.log(eng_translation);
+      } catch (error) {
+        console.log(error);
+        
+        
+        col.innerHTML = `
+        
+        <div class="card text-white bg-light mb-3 h-100 mt-3 shadow-lg" >
+        
+        <div class="card-body text-center bg-transparent">
+        <div class="card-text text-dark">Something Went Wrong</div>
+        <a onclick="load()" class="btn btn-lg btn-transparent"><i class="fa-solid fa-rotate-right"></i> Click to Refresh</a>
+        
+      </div>
+
+ </div>`;
+    row.append(col);
   }
 }
 getData()
